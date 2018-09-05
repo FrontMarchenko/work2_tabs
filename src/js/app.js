ad94d70e7ddffd9b79428f33cedbdbfc5a0eb239
VolodymyrMarchenko <nokialeader@gmail.com>
2018-09-05 15:03:25 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,77 @@
+checkHash('.main__tabs');
+
 $('.tab').on('click', function() {
   clickTab(true, $(this));
-  return false;
+  // return false;
 });
 
 $(window).on('hashchange', function(e) {
-  var link = {};
-  if (location.hash !== '') {
-    link = $('.tab[href="' + location.hash + '"]');
-  } else {
-    link = $('.tab.first');
-  }
-  clickTab(false, link);
+  checkHash('.main__tabs');
 });
 
-function clickTab(real, link) {
-  if (link.hasClass('tab')) {
-    var id = link.attr('href');
-    link.addClass('is-active')
+function clickTab(real, hash) {
+  if (hash.hasClass('tab')) {
+    var id = hash.data('link');
+    hash.addClass('is-active')
       .siblings().removeClass('is-active');
-    $(id).removeClass('is-hidden')
-      .siblings().addClass('is-hidden');
+    $(id).addClass('is-visible')
+      .siblings().removeClass('is-visible');
     if (real) {
       history.pushState(null, null, id);
     }
   } else {
     return false;
-  }
-}
+  };
+};
+
+function checkHash(tabs) {
+  var hash = {};
+
+  $(tabs).each(function(i, e) {
+    e = $(e);
+    if (location.hash !== '') { 
+      hash = e.find('.tab[data-link="' + location.hash + '"]');
+      if (hash.length === 0 ) {
+        hash = e.find('.tab.first');
+      }
+    } else {
+      hash = e.find('.tab.first');
+    }
+        
+    clickTab(false, hash);
+  });
+
+};
+
+
+// $('.tab').on('click', function() {
+//   clickTab(true, $(this));
+//   return false;
+// });
+
+// $(window).on('load hashchange', function(e) {
+//   var hash = {};
+//   if (location.hash !== '') {
+//     hash = $('.tab[data-link="' + location.hash + '"]');
+//   } else {
+//     hash = $('.tab.first');
+//   }
+//   clickTab(false, hash);
+// });
+
+// function clickTab(real, hash) {
+//   if (hash.hasClass('tab')) {
+//     var id = hash.data('link');
+//     hash.addClass('is-active')
+//       .siblings().removeClass('is-active');
+//     $(id).removeClass('block')
+//       .siblings().addClass('block');
+//     $(id).addClass('is-active')
+//       .siblings().removeClass('is-active');
+//     if (real) {
+//       history.pushState(null, null, id);
+//     }
+//   } else {
+//     return false;
+//   }
+// }
